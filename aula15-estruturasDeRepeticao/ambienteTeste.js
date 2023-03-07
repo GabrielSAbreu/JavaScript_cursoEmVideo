9b061ed860af3bbfccafbbf2921e6214b104d603
Gabriel Abreu <87873067+GabrielSAbreu@users.noreply.github.com>
2023-03-07 10:21:42 -0300

    repeticao

---
+++ aula15-estruturasDeRepeticao/ambienteTeste.js
@@ -0,0 +1,6 @@
+console.log('Tudo bem!')
+let cont = 1
+while(cont <10){
+    console.log(`Tudo ${cont}`)
+    cont++
+}
---
+++ aula15-estruturasDeRepeticao/node_8ce5dfe6cc987.tmp.js
@@ -0,0 +1,5 @@
+console.log('Tudo bem!')
+let cont = 1
+while(cont <10){
+    console.log(`Tudo ${cont}`)
+}
